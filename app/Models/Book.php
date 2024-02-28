2c0bfe91bf85242723e72d1c139afe582d9bb5aa
George Kalandadze <kalandadzeg777@gmail.com>
2024-02-29 00:56:34 +0400

    feat/write eloquent relationship between author and book

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Book extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'publish_date',
+        'status',
+    ];
+
+    public function authors(): BelongsToMany
+    {
+        return $this->belongsToMany(Author::class);
+    }
 }
